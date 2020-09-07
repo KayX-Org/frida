b52f76a6ee9ead4cde6e2b6d84507c84b39c11ae
Diego Nava <diego.nava77@hotmail.com>
2020-09-07 08:49:31 +0200

    Remove the dependcy to freja

--- frida.go
+++ frida.go
@@ -3,17 +3,21 @@ package frida
 import (
 	"context"
 	"fmt"
-	"github.com/kayx-org/freja"
-	"github.com/kayx-org/freja/healthcheck"
 	"github.com/kayx-org/frida/queue"
 	"github.com/kayx-org/frida/topology"
 	"github.com/streadway/amqp"
 	"os"
 )
 
+type logger interface {
+	Errorf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+}
+
 type topologyCreator interface {
 	CreateTopology(ctx context.Context) error
 }
+
 type Handler func(*Message) error
 
 type Frida struct {
@@ -22,10 +26,10 @@ type Frida struct {
 	handlers        map[Topic]Handler
 	connection      *amqp.Connection
 	bucket          string
-	logger          freja.Logger
+	logger          logger
 }
 
-func New(logger freja.Logger) (*Frida, error) {
+func New(logger logger) (*Frida, error) {
 	conn, err := amqp.Dial(getEnv("RABBITMQ_HOST", "amqp://guest:guest@localhost:5672/"))
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect: %w", err)
@@ -103,32 +107,36 @@ func (f *Frida) runHandler(_ context.Context, topic Topic, handler Handler) erro
 		return fmt.Errorf("unable to start consuming: %w", err)
 	}
 
-	go func() {
-		defer func() {
-			// If one of the channels stop consuming we will close all of the rest
-			f.consuming = false
-			_ = channel.Close()
-		}()
-		for m := range msg {
-			if !f.consuming {
-				return
-			}
-
-			translatedMsg := NewMessage(m.Body, Topic(m.RoutingKey))
-			translatedMsg.timestamp = m.Timestamp
-			if err := handler(translatedMsg); err != nil {
-				if err := m.Nack(false, false); err != nil {
-					f.logger.Errorf("unable to nack topic '%s': %s", m.RoutingKey, err)
-				}
-			} else {
-				if err := m.Ack(false); err != nil {
-					f.logger.Errorf("unable to ack topic '%s': %s", m.RoutingKey, err)
-				}
-			}
-		}
-	}()
+	go f.consume(msg, channel, handler, topicQueue.Name())
 
 	return nil
+}
+
+func (f *Frida) consume(msg <-chan amqp.Delivery, channel *amqp.Channel, handler Handler, topicName string) {
+	defer func() {
+		// If one of the channels stop consuming we will close all of the rest
+		f.consuming = false
+		_ = channel.Close()
+	}()
+
+	f.logger.Infof("start consuming for topic '%s'", topicName)
+	for m := range msg {
+		if !f.consuming {
+			return
+		}
+
+		translatedMsg := NewMessage(m.Body, Topic(m.RoutingKey))
+		translatedMsg.timestamp = m.Timestamp
+		if err := handler(translatedMsg); err != nil {
+			if err := m.Nack(false, false); err != nil {
+				f.logger.Errorf("unable to nack topic '%s': %s", m.RoutingKey, err)
+			}
+		} else {
+			if err := m.Ack(false); err != nil {
+				f.logger.Errorf("unable to ack topic '%s': %s", m.RoutingKey, err)
+			}
+		}
+	}
 }
 
 func (f *Frida) Stop(_ context.Context) error {
@@ -136,16 +144,8 @@ func (f *Frida) Stop(_ context.Context) error {
 	return f.connection.Close()
 }
 
-func (f *Frida) Name() string {
-	return "frida"
-}
-
-func (f *Frida) Status() healthcheck.ServiceStatus {
-	if f.consuming && !f.connection.IsClosed() {
-		return healthcheck.UP
-	}
-
-	return healthcheck.DOWN
+func (f *Frida) IsRunning() bool {
+	return f.consuming && !f.connection.IsClosed()
 }
 
 func getEnv(key string, defaultVal string) string {
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/kayx-org/frida
 
 go 1.15
 
-require (
-	github.com/kayx-org/freja v0.8.4 // indirect
-	github.com/streadway/amqp v1.0.0
-)
+require github.com/streadway/amqp v1.0.0
